c0ea4b1b0f7424d1dfd698b93a15dac16a4f5a9f
unknown <kunalkeshri047@gmail.com>
2020-08-01 22:21:04 +0530

    Removed Register and Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,28 +12,25 @@ import { ToastrModule } from 'ngx-toastr';
 
 
 const routes: Routes = [
+  // {
+  //   path:"",
+  //   component:RegisterComponent
+  // },
+  // {
+  //   path:"login",
+  //   component:LoginComponent
+  // },
   {
     path:"",
-    component:RegisterComponent
-  },
-  {
-    path:"login",
-    component:LoginComponent
-  },
-  {
-    path:"dashboard",
-    canActivate:[ActivateGuard],
     component:DashboardComponent,
 
   },
   {
     path:"state",
-    canActivate:[ActivateGuard],
     component:StateComponent
   },
   {
     path:"district/:code",
-    canActivate:[ActivateGuard],
     component:DistrictComponent
   }
 ];
